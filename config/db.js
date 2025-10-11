00071f38dd1fa5e636ac92cd561b8826ab998f5d
waqas1343 <227280721+waqas5904@users.noreply.github.com>
2025-10-11 12:00:24 -0700

    login screen api setting

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // Use the MONGO_URI environment variable
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['user', 'admin'],
+      enum: ['user', 'admin', 'gas_plant', 'distributor'],
       default: 'user',
     },
   },
